d91ff0c2cfafb15fd6ad1e35fc3e8db88ecb7626
Rambabu Bodrothu <142653098+Rambabu606@users.noreply.github.com>
2025-05-28 23:27:20 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,24 @@
+public class Program{
+    public static void main(String[] args) {
+        byte b= 10;
+        short s= 200;
+        int a=1000;
+        long l=100000L;
+        float f= 10.5f;
+        double d= 20.99;
+        char ch='A';
+        boolean bh= true;
+        String st= "Hello,Java!";
+               System.out.println(b);
+               System.out.println(s);
+               System.out.println(a);
+               System.out.println(l);
+               System.out.println(f);
+               System.out.println(d);
+               System.out.println(ch);
+               System.out.println(bh);
+               System.out.println(st);
+
+               
+    }
+}
